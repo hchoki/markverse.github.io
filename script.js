ede1e4a5980a15f27a0bd40fbc12427dc72731a1
choki <choki@pawlygon.net>
2023-06-27 18:23:14 -0400

    Add new photos

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 window.addEventListener('DOMContentLoaded', function() {
     var slideshowContainer = document.getElementById('slideshow-container');
     var imageUrls = [
+        'images/paris1.png',
         'images/titanic4.png',
         'images/Donkeysaur.png',
+        'images/paris3.png',
         'images/Mark-Zuckerberg-Spooks-the-Internet-With-Too-Much-Sunscreen-on-His-Face-in-Hawaii-01.jpeg',
         'images/image1.png',
         'images/mouthplay2.png',
         'images/image.png',
         'images/titanic3.png',
+        'images/paris4.png',
         'images/VRChat_2023-05-27_02-27-31.591_1920x1080.png',
         'images/mouthplay3.png',
         'images/nerdy1.png',
         'images/VRChat_2023-06-02_05-32-49.894_1920x1080.png',
         'images/titanic2.png',
         'images/nerdy2.png',
+        'images/paris2.png',
         'images/mouthplay1.png',
         'images/VRChat_2023-06-03_00-20-16.117_1920x1080.png',  
         'images/titanic1.png',

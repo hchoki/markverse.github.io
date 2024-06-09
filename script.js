3991c2518914c1d69ac18365d3943ed10e24d7b3
Choki <choki@pawlygon.net>
2024-06-09 18:05:09 -0400

    Umbra Day 3

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 window.addEventListener('DOMContentLoaded', function() {
     var slideshowContainer = document.getElementById('slideshow-container');
     var imageUrls = [
+        'images/umbra1.png',
         'images/VRChat_2024-06-07_22-11-09.png',
+        'images/umbra2.png',
         'images/VRChat_2024-06-08_20-57-33.018_2560x1440.png',
+        'images/umbra3.png',
         'images/VRChat_2024-06-07_01-37-34.697_1920x1080.png',
         'images/VRChat_2024-06-08_21-00-08.892_2560x1440.png',
         'images/hecka.png',
